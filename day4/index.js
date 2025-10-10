c430ec9e9d77a44aba88d8303ec81e058e7ce50c
shivlee <shivlee@shivlees-MacBook-Air.local>
2025-10-10 17:11:48 +0530

    update files

---
+++ day4/index.js
@@ -0,0 +1,36 @@
+//number
+// let a=10;
+// let b=345.6821;
+// console.log(b);
+// console.log(b.toFixed(2));   // sirf 2 digit tak devimal uthayega
+
+//it returns string and create a new string dont change b
+
+// console.log(b.toPrecision(4));    //gives precise value
+
+// console.log(b.toString());   //convert to string
+
+// let a = new Number(20);     //new number created  //type is object
+// let b=20;
+// console.log(a);
+// console.log(b);
+// console.log(typeof a);     //object
+// console.log(typeof b);      //number
+
+//non primitive data reference k base pe compare hote h
+//primitive copy by value
+
+// console.log(Math.abs(-4));   //gives absolute value
+
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.ceil(2.3));
+
+// console.log(Math.random());    //generates a random value between[0,1)
+
+console.log(Math.floor(Math.random()*10 +1));   //generate value *10
+//floor is used to get an integer value.....for removing decimals
+
+//Math.floor(Math.random()*(max-min+1))+min
+//we use crypto library to generate random number bcoz this is not safe and can be hacked 
+
