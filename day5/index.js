b1dc036833d29016ea169a848ca95e9fb5faaea8
shivlee <shivlee@shivlees-MacBook-Air.local>
2025-10-11 14:37:12 +0530

    update files

---
+++ day5/index.js
@@ -0,0 +1,41 @@
+//string
+// const str1 = "Rohit";
+// const str2 = 'Rohit Negi';
+// const day  =18;
+// const str3 = `Strike is 
+// coming soon on ${day}`;               //we can write it in different lines
+
+// console.log(str1,str2,str3);
+
+
+// const str = `Hello Coder Army`;
+// console.log(str.length);            //space is also counted
+// console.log(str[0]);                //strings are immutable->cant change
+
+// //upper case
+// console.log(str.toUpperCase());    //we are given a new string it dont change in original string
+// console.log(str);
+
+// console.log(str.indexOf('Cod')); 
+// console.log(str.includes('Cod'));
+
+// console.log(str.slice(2,5));           // it gives the letters frim 2 to 4 ,5 is not included
+// console.log(str.slice(-4));            // last k 4
+
+// console.log(str.substring(2,5));        //same as slice but we cant give -ve index
+
+// const a = "Rohit";
+// const b = "Negi";
+// const d = " ";
+// const c = a+d+b;
+// console.log(c);             //it get attached
+
+// const str = `Hello Coder Army`;
+// console.log(str.replace("ode" , 'iam'));
+// const user = " Rohit ";
+// console.log(user.trim());        ->remove front and back space
+
+const names = "Rohit,Mohit,Suraj,Rohan,Anjali";
+
+console.log(names.split(","));
+
