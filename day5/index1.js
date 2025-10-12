a810dc8ced54ddc228edd530e7c918ada7567bc8
shivlee <shivlee@shivlees-MacBook-Air.local>
2025-10-12 13:02:48 +0530

    update file

---
+++ day5/index1.js
@@ -0,0 +1,15 @@
+//Date
+
+// const now = new Date();
+// console.log(now);               //gives utc time (universal time coordinate)
+// console.log(now.toString());    //(India Standard Time)
+
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getFullYear());
+// console.log(now.getHours());
+// console.log(now.getMonth());        //month start on 0base indexing
+
+const now = new Date(2025,9,11,2,46,5,125);
+//year month date hour min sec millisec
+console.log(now.toString());
